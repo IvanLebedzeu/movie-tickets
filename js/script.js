1e24b24498feaf0ae1f0cead9c7162b69834dbf6
Steven Ly <1stevenly92@gmail.com>
2020-04-07 10:01:26 -0700

    added if statement to decide the price

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
-//Business Logic
-function MovieTicket(){
+//Business Logic for ticket
+function Ticket(userAge, userTime, userMovie){
+    this.userAge = userAge;
+    this.userTime = userTime;
+    this.userMovie = userMovie;
     
+}
+
+Ticket.prototype.ticketCost = function() {
+  var price = 0;
+
+  if (this.userAge === "mantiee" && this.userTime === "am") {
+    price += 20;
+    console.log(price);
+
+  } else if ((this.userAge === "senior" || this.userAge === "kids") && this.userTime === "am"){
+    price += 10;
+
+  } else if (this.userAge === "mantiee" && this.userTime === "pm") {
+    price += 25;
+  } else if ((this.this.userAge === "senior" || this.userAge === "kids") && this.userTime === "pm") {
+    price += 15;
+  }
+  
+  
+return ("At " + this.Time + "you gonna see " + this.Movie + "." + "Your ticket cost is: " + price);
+
+ 
 }
 
 
@@ -32,6 +57,14 @@ $(document).ready(function(){
     var userAge = $("#age").val();
     var userMovie = $("#movie").val();
     console.log(userTime, userAge, userMovie);
+    
+    //empty fields after submission
+    $("#time").val('');
+    $("#age").val('');
+    $("#movie").val('');
+
+    var userTicket = new Ticket (userTime, userAge, userMovie);
+
   })
     
 
